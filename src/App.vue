65eb2cb3374ce2d3c678f9d9b6da025afea7ae7e
Quynhb1910568 <ngoquynh2908@gmail.com>
2023-10-31 14:35:18 +0700

    Khoi tao du an

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<script>
+//import HelloWorld from './components/HelloWorld.vue'
+//import TheWelcome from './components/TheWelcome.vue'
+export default{
+
+}
+</script>
+
+<template>
+  <h1>Hello, Vue.js!</h1>
+</template>
+
+<style>
+  .page {
+    max-width: 400px;
+    margin:auto;
+  }
+</style>
